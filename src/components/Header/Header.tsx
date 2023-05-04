725d6f956ff5a963a4e184cf75e69c44d36a404c
divyaraj <divyarajbaraiya05@gmail.com>
2023-05-04 16:28:05 +0530

    footer design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { FC, useContext } from "react";
+import { TbFileDollar } from "react-icons/tb";
+import { BsPersonCheck } from "react-icons/bs";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 import "./assets/style.scss";
 
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts";
 import { AppHeaderLogo } from "../AppHeaderLogo";
+import { AppButton } from "../AppButton";
 
 interface HeaderProps {}
 
@@ -23,6 +27,32 @@ export const Header: FC<HeaderProps> = () => {
       <AppHeaderLogo />
 
       <div className="header-container--action">
+        <div className="header-nav">
+          <BsPersonCheck />
+          <span className="header-nav--title">Browse Talent</span>
+        </div>
+
+        <div className="header-nav">
+          <TbFileDollar />
+          <span className="header-nav--title">Pricing</span>
+        </div>
+
+        <div className="header-btn">
+          <AppButton
+            type="button"
+            variant="secondary"
+            className="post-btn"
+            onClick={() => navigate("/dummy")}
+          >
+            Post a Job
+          </AppButton>
+        </div>
+
+        <div className="header-notification-badge">
+          <IoMdNotificationsOutline className="notification-icon" />
+          <div className="notification-span">0</div>
+        </div>
+
         <div className="header-logout">
           <div className="logout-icon" onClick={handleLogout} />
         </div>
